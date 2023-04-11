27f0606a43477b40312eed87b3509c78cf8a84de
SimoneFerrini <ferrini.simone4@gmail.com>
2023-04-11 11:54:14 +0200

    init + import/install

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <script>
+  import {store} from "./store";
+  import AppNavbar from "./components/AppNavbar.vue";
+  import AppHeader from "./components/AppHeader.vue";
+  import AppMain from "./components/AppMain.vue";
+  
 
+  export default{
+  data(){
+    return{
+
+        store,
+    }
+  },
+
+  components:{
+    AppNavbar,
+    AppHeader,
+    AppMain,
+  },
+}
 </script>
 
 <template>
