a956f33af129d5ded7dc41219488749768547e6e
SimoneFerrini <ferrini.simone4@gmail.com>
2023-04-13 15:40:54 +0200

    ended dinamic footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -26,7 +26,33 @@
 <template>
     <div class="bgcolor">
         <div class="footer-top">
-            ciao
+            <div class="container">
+                <div class="col first">
+                    <div class="img-container">
+                        <img src="/img/logo.png" alt="img">
+                    </div>
+                    <p class="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia aliquid ea eos excepturi minus tempora maiores illum exercitationem corporis nulla quae et ullam, maxime obcaecati repellendus non autem veniam.</p>
+                    <ul>
+                        <li v-for="icon in store.iconSocial">
+                            <a href=""><i :class="icon"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col second">
+                    <h3>Recent Posts</h3>
+                    <ul>
+                        <li v-for="item in store.recentPosts">
+                            <a href=""><i class="fa-solid fa-angle-right"></i> {{ item }}</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col third">
+                    <h3>Contact Information</h3>
+                    <span class="location">{{ store.footerContact.location }}</span>
+                    <span class="contact">Mobile: {{store.footerContact.mobile}}</span>
+                    <span class="contact">Email: {{store.footerContact.email}}</span>
+                </div>
+            </div>
         </div>
         <div class="footer-bot">
             <div class="container">
@@ -48,7 +74,108 @@
     .bgcolor{
         background-color: $paletteBlack;
         color: $paletteWhite;
+       
+        //top-----------------------------------------------------------------------
+        .footer-top{
+            .container{
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
 
+                padding: 9em 0;
+
+                .col{
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    text-align: center;
+                    gap: 1.5em;
+
+                    width: calc(100% / 3 - 60px / 3 * 2);
+                }
+
+                //prima colonna - sinistra --------------------------------------------
+                .col.first{
+                    .img-container{
+                        width: 80%;
+
+                        img{
+                            width: 100%;
+                        }
+                    }
+
+                    .text{
+                        font-size: 0.9em;
+                        font-weight: 500;
+                        color: $paletteGrey;
+                    }
+
+                    ul{
+                        display: flex;
+                        flex-direction: row;
+                        gap: 1em;
+                        list-style-type: none;
+
+                        li{
+                            a{
+                                color: $paletteRed;
+                            }
+                        }
+
+                    }
+                }
+                //secondo colonna - centrale ------------------------------------------
+                .col.second{
+                    h3{
+                        font-weight: 400;
+                        text-transform: uppercase;
+                        margin-bottom: 2em;
+                    }
+
+                    ul{
+                        display: flex;
+                        flex-direction: column;
+                        gap: 1.5em;
+                        list-style: none;
+
+                        li{
+                            a{
+                                color: $paletteWhite;
+                                text-decoration: none;
+                            }
+                        }
+                    }
+                }
+
+                //terza colonna - destra---------------------------------------------
+                .col.third{
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1em;
+
+                    h3{
+                        font-weight: 400;
+                        text-transform: uppercase;
+                        margin-bottom: 2em;
+                    }
+
+                    .location{
+                        color: $paletteGrey;
+                        font-weight: 400;
+                        margin-bottom: 2em;
+                    }
+
+                    .contact{
+                        font-size: 1.1em;
+                        color: $paletteWhite;
+                        font-weight: 800;
+                    }
+                }
+
+
+            }
+        }
+        //bot-----------------------------------------------------------------------
         .footer-bot{
             width: 100%;
             border-top: 1px solid $paletteGrey;
